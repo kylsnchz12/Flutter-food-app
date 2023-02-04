6d16202e9a568d40e579320e8d727f5efc11a8b3
Kyle <kyle_sanchez99@yahoo.com>
2023-02-04 15:41:21 +0800

    added banner

--- flutter_foodui/lib/data.dart
+++ flutter_foodui/lib/data.dart
@@ -4,3 +4,34 @@ List<String> demoBigImages = [
   "assets/images/big_3.png",
   "assets/images/big_4.png"
 ];
+
+List<Map<String, dynamic>> demoMediumCardData = [
+  {
+    "name": "Daylight Coffee",
+    "image": "assets/images/medium_1.png",
+    "location": "Colorado, San Francisco",
+    "rating": 4.6,
+    "deliveryTime": 25,
+  },
+  {
+    "name": "Daylight Coffee 2",
+    "image": "assets/images/medium_2.png",
+    "location": "Colorado, San Francisco",
+    "rating": 4.6,
+    "deliveryTime": 25,
+  },
+  {
+    "name": "Daylight Coffee 3",
+    "image": "assets/images/medium_3.png",
+    "location": "Colorado, San Francisco",
+    "rating": 4.6,
+    "deliveryTime": 25,
+  },
+  {
+    "name": "Daylight Coffee 4",
+    "image": "assets/images/medium_4.png",
+    "location": "Colorado, San Francisco",
+    "rating": 4.6,
+    "deliveryTime": 25,
+  },
+];
--- flutter_foodui/lib/home_screen.dart
+++ flutter_foodui/lib/home_screen.dart
@@ -16,8 +16,9 @@ class _HomeScreenState extends State<HomeScreen> {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
-            backgroundColor: Colors.transparent,
+            backgroundColor: Colors.white,
             elevation: 0,
+            floating: true,
             title: Column(children: [
               Text(
                 "Delivery to".toUpperCase(),
@@ -40,32 +41,150 @@ class _HomeScreenState extends State<HomeScreen> {
                   ))
             ],
           ),
-          SliverToBoxAdapter(
-            child: AspectRatio(
-              aspectRatio: 1.81,
-              child: Stack(
-                alignment: Alignment.bottomRight,
-                children: [
-                  PageView.builder(
-                      itemCount: demoBigImages.length,
-                      itemBuilder: (context, index) =>
-                          Image.asset(demoBigImages[index])),
-                  Positioned(
-                      bottom: 16,
-                      right: 16,
-                      child: Row(
-                        children: List.generate(
-                            demoBigImages.length,
-                            (index) => Padding(
-                                  padding: const EdgeInsets.only(left: 16 / 4),
-                                  child: IndicatorDot(isActive: index == 1),
-                                )),
-                      ))
-                ],
+          const SliverPadding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              sliver: SliverToBoxAdapter(child: ImageCarousel())),
+          SliverPadding(
+            padding: const EdgeInsets.all(16),
+            sliver: SliverToBoxAdapter(
+              child: SectionTitle(
+                title: "Featured Partners",
+                press: () {},
               ),
             ),
-          )
+          ),
+          SliverToBoxAdapter(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                  children: List.generate(
+                demoMediumCardData.length,
+                (index) => Padding(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: RestaurantInfoMediumCard(
+                    title: demoMediumCardData[index]['name'],
+                    location: demoMediumCardData[index]['location'],
+                    image: demoMediumCardData[index]['image'],
+                    deliveryTime: demoMediumCardData[index]['deliveryTime'],
+                    rating: demoMediumCardData[index]['rating'],
+                    press: () {},
+                  ),
+                ),
+              )),
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.all(16),
+            sliver: SliverToBoxAdapter(
+              child: Image.asset("assets/images/Banner.png"),
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.all(16),
+            sliver: SliverToBoxAdapter(
+              child: SectionTitle(
+                title: "Best Pick",
+                press: () {},
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                  children: List.generate(
+                demoMediumCardData.length,
+                (index) => Padding(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: RestaurantInfoMediumCard(
+                    title: demoMediumCardData[index]['name'],
+                    location: demoMediumCardData[index]['location'],
+                    image: demoMediumCardData[index]['image'],
+                    deliveryTime: demoMediumCardData[index]['deliveryTime'],
+                    rating: demoMediumCardData[index]['rating'],
+                    press: () {},
+                  ),
+                ),
+              )),
+            ),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class RestaurantInfoMediumCard extends StatelessWidget {
+  const RestaurantInfoMediumCard({
+    super.key,
+    required this.title,
+    required this.image,
+    required this.location,
+    required this.deliveryTime,
+    required this.rating,
+    required this.press,
+  });
+
+  final String title, image, location;
+  final int deliveryTime;
+  final double rating;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: press,
+      borderRadius: const BorderRadius.all(Radius.circular(6)),
+      child: SizedBox(
+        width: 200,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AspectRatio(
+              aspectRatio: 1.25,
+              child: Image.asset(image),
+            ),
+            const SizedBox(height: 16 / 2),
+            Text(
+              title,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            Text(
+              location,
+              style: const TextStyle(color: Colors.grey),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16 / 2),
+              child: DefaultTextStyle(
+                style: const TextStyle(color: Colors.black, fontSize: 12),
+                child: Row(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16 / 2, vertical: 16 / 8),
+                      decoration: const BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.all(Radius.circular(6))),
+                      child: Text(
+                        rating.toString(),
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    const Spacer(),
+                    Text("$deliveryTime mins"),
+                    const Spacer(),
+                    const CircleAvatar(
+                        radius: 2, backgroundColor: Color(0xFF868686)),
+                    const Spacer(),
+                    const Text("Free Delivery")
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -84,8 +203,81 @@ class IndicatorDot extends StatelessWidget {
       height: 4,
       width: 8,
       decoration: BoxDecoration(
-          color: isActive ? Colors.white : Colors.white54,
+          color: isActive ? Colors.white : Colors.white38,
           borderRadius: const BorderRadius.all(Radius.circular(12))),
+    );
+  }
+}
+
+class ImageCarousel extends StatefulWidget {
+  const ImageCarousel({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<ImageCarousel> createState() => _ImageCarouselState();
+}
+
+class _ImageCarouselState extends State<ImageCarousel> {
+  int _currentPage = 0;
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1.81,
+      child: Stack(
+        alignment: Alignment.bottomRight,
+        children: [
+          PageView.builder(
+              itemCount: demoBigImages.length,
+              onPageChanged: (value) {
+                setState(() {
+                  _currentPage = value;
+                });
+              },
+              itemBuilder: (context, index) => ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(12)),
+                  child: Image.asset(demoBigImages[index]))),
+          Positioned(
+              bottom: 16,
+              right: 16,
+              child: Row(
+                children: List.generate(
+                    demoBigImages.length,
+                    (index) => Padding(
+                          padding: const EdgeInsets.only(left: 16 / 4),
+                          child: IndicatorDot(isActive: index == _currentPage),
+                        )),
+              ))
+        ],
+      ),
+    );
+  }
+}
+
+class SectionTitle extends StatelessWidget {
+  const SectionTitle({
+    Key? key,
+    required this.title,
+    required this.press,
+  }) : super(key: key);
+
+  final String title;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        TextButton(
+            onPressed: press,
+            style: TextButton.styleFrom(foregroundColor: Colors.green),
+            child: const Text("Sell all"))
+      ],
     );
   }
 }
